de0611993e619cee1ba38bb24330e1bc393d15df
robnagler <github@q33.us>
2014-12-28 18:13:01 -0700

    change directories to 2014/12-25 so broken up by year

--- macutil/dropbox_photos_rename.py
+++ macutil/dropbox_photos_rename.py
@@ -11,12 +11,15 @@ def move_one(src):
     dst_base = src_base.replace(' ', '-').lower().replace('.jpeg', '.jpg')
     match = re.search('(20\d\d)[_-](\d\d)[_-](\d\d)', dst_base)
     if match:
-        dst_dir = '-'.join([match.group(1), match.group(2), match.group(3)])
+        dst_dir = match.group(1) + '/' + match.group(2) + '-' + match.group(3)
     else:
         dt = datetime.datetime.fromtimestamp(os.path.getmtime(src))
-        dst_dir = dt.strftime('%Y-%m-%d')
+        dst_dir = dt.strftime('%Y/%m-%d')
     dst_dir = os.path.join(os.environ['HOME'], 'Dropbox', 'Photos', dst_dir)
     if not os.path.isdir(dst_dir):
+        parent = os.path.dirname(dst_dir)
+        if not os.path.isdir(parent):
+            os.mkdir(parent)
         os.mkdir(dst_dir)
     os.rename(src, os.path.join(dst_dir, dst_base))
 
